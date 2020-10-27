235ea9525c16399d86ce0c737a06ebde9433995f
sjydavid <holymackerelisop@gmail.com>
2020-10-27 17:05:34 +0900

    Initial commit

---
+++ inc/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta property="og:title" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:url" content="" />
+    <meta name="twitter:card" content="" />
+    
+	<!-- Fav and touch icons -->
+	<link rel="shortcut icon" href="">
+	
+	<!-- css -->
+    <link href="/assets/css/common.css" rel="stylesheet" type="text/css">
+    <link href="/assets/css/layout.css" rel="stylesheet" type="text/css">
+	<link href="/assets/css/aos.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+	<!-- js -->
+	<script src="/assets/js/jquery-3.5.1.min.js"></script>
+    <script src="/assets/js/common.js"></script>
+    <script src="/assets/js/aos.js"></script>
---
+++ index.php
@@ -0,0 +1,10 @@
+<? include $_SERVER['DOCUMENT_ROOT'].'/inc/head.php'; ?>
+	<title></title>
+</head>
+<body>
+<? include $_SERVER['DOCUMENT_ROOT'].'/inc/header.php'; ?>
+	<section>
+
+
+	</section>
+<? include $_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'; ?>
