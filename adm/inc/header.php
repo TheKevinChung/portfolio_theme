d988614d1de5ecbcc982547004f345b7ea6457dd
TheKevinChung <holymackerelisop@gmail.com>
2020-11-02 13:49:08 +0900

    Fix code style

--- adm/inc/header.php
+++ adm/inc/header.php
@@ -12,59 +12,62 @@
 	</li>
 	<hr class="sidebar-divider">
 	<div class="sidebar-heading">
-	Setting
+		Setting
 	</div>
 	<li class="nav-item">
-		<a class="nav-link" href="#none">
+		<a class="nav-link" href="/adm/board/config/">
 			<i class="fas fa-cogs"></i>
 			<span>default setting</span>
 		</a>
 	</li>
 	<hr class="sidebar-divider">
 </ul>
-    <!-- Sidebar -->
-    <div id="content-wrapper" class="d-flex flex-column">
-      <div id="content">
-        <!-- TopBar -->
-        <nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top">
-          <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                aria-labelledby="searchDropdown">
-                <form class="navbar-search">
-                  <div class="input-group">
-                    <input type="text" class="form-control bg-light border-1 small" placeholder="What do you want to look for?"
-                      aria-label="Search" aria-describedby="basic-addon2" style="border-color: #3f51b5;">
-                    <div class="input-group-append">
-                      <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </li>
-            <div class="topbar-divider d-none d-sm-block"></div>
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                <img class="img-profile rounded-circle" src="img/boy.png" style="max-width: 60px">
-                <span class="ml-2 d-none d-lg-inline text-white small"><?=$_SESSION['adm_nick']?></span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="javascript:void(0);" data-toggle="modal" data-target="#logoutModal">
-                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Logout
-                </a>
-              </div>
-            </li>
-          </ul>
-        </nav>
-        <!-- Topbar -->
+<!-- Sidebar -->
+<div id="content-wrapper" class="d-flex flex-column">
+	<div id="content">
+		<!-- TopBar -->
+		<nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top">
+			<button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
+				<i class="fa fa-bars"></i>
+			</button>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item dropdown no-arrow">
+					<a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+						data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<i class="fas fa-search fa-fw"></i>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+						aria-labelledby="searchDropdown">
+						<form class="navbar-search">
+							<div class="input-group">
+								<input type="text" class="form-control bg-light border-1 small"
+									placeholder="What do you want to look for?" aria-label="Search"
+									aria-describedby="basic-addon2" style="border-color: #3f51b5;">
+								<div class="input-group-append">
+									<button class="btn btn-primary" type="button">
+										<i class="fas fa-search fa-sm"></i>
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</li>
+				<div class="topbar-divider d-none d-sm-block"></div>
+				<li class="nav-item dropdown no-arrow">
+					<a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+						aria-haspopup="true" aria-expanded="false">
+						<img class="img-profile rounded-circle" src="/adm/img/boy.png" style="max-width: 60px">
+						<span class="ml-2 d-none d-lg-inline text-white small"><?=$_SESSION['adm_nick']?></span>
+					</a>
+					<div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+						aria-labelledby="userDropdown">
+						<a class="dropdown-item" href="javascript:void(0);" data-toggle="modal"
+							data-target="#logoutModal">
+							<i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+							Logout
+						</a>
+					</div>
+				</li>
+			</ul>
+		</nav>
+		<!-- Topbar -->
--- adm/index.php
+++ adm/index.php
@@ -11,27 +11,27 @@
 </head>
 
 <body id="page-top">
-  <div id="wrapper">
-  <? include $_SERVER['DOCUMENT_ROOT'].'/adm/inc/header.php'; ?>
-        <!-- Container Fluid-->
-        <div class="container-fluid" id="container-wrapper">
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="./">Home</a></li>
-              <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
-            </ol>
-          </div>
+	<div id="wrapper">
+		<? include $_SERVER['DOCUMENT_ROOT'].'/adm/inc/header.php'; ?>
+		<!-- Container Fluid-->
+		<div class="container-fluid" id="container-wrapper">
+			<div class="d-sm-flex align-items-center justify-content-between mb-4">
+				<h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item"><a href="./">Home</a></li>
+					<li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+				</ol>
+			</div>
 
-          <div class="row mb-3">
-            <div class="text-center col-12">
-              <img src="img/think.svg" style="max-height: 90px">
-              <h4 class="pt-3">save your <b>imagination</b> here!</h4>
-            </div>
-          </div>
-          <!--Row-->
-        </div>
-        <!---Container Fluid-->
-      </div>
+			<div class="row mb-3">
+				<div class="text-center col-12">
+					<img src="img/think.svg" style="max-height: 90px">
+					<h4 class="pt-3">save your <b>imagination</b> here!</h4>
+				</div>
+			</div>
+			<!--Row-->
+		</div>
+		<!---Container Fluid-->
+	</div>
 
-<? include $_SERVER['DOCUMENT_ROOT'].'/adm/inc/footer.php'; ?>
+	<? include $_SERVER['DOCUMENT_ROOT'].'/adm/inc/footer.php'; ?>
