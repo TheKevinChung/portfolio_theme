4c49f64436a8b5df5b206cddd9bf1405ff4cbb3c
TheKevinChung <holymackerelisop@gmail.com>
2020-11-04 16:23:45 +0900

    Update DB connect info

--- lib/database.php
+++ lib/database.php
@@ -59,7 +59,7 @@ function libConnect() {
 	global $mysqli;
 
 	if (!$mysqli) {
-		$mysqli = @mysqli_connect('localhost', 'chungkevin', 'chungkevin#!@', 'chungkevin') or include('err_db.php');
+		$mysqli = @mysqli_connect('db.chungkevin.com', 'chungkevin', 'chungkevin!@#', 'dbchungkevin') or include('err_db.php');
 		@mysqli_set_charset($mysqli, 'utf8');
 	}
 	return $mysqli ;
