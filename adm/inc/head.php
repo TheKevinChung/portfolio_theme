48838605c34367619110ec290ed95516fcde22ff
TheKevinChung <holymackerelisop@gmail.com>
2020-11-04 19:01:57 +0900

    Fix admin img style

--- adm/inc/head.php
+++ adm/inc/head.php
@@ -11,6 +11,6 @@
   <link href="/adm/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="/adm/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
   <link href="/adm/css/ruang-admin.min.css" rel="stylesheet">
-  <link href="/adm/css/style.css" rel="stylesheet">
+  <link href="/adm/css/style.css?ver=0.2" rel="stylesheet">
 
   <title>Admin</title>
